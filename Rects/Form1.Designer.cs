f8dd2973cd7f47955e6335e9c6ca63008c8f0385
Myron Marintsev <marintsev@gmail.com>
2016-03-13 02:31:36 +0400

    Делаю отрисовку.

--- Rects/Form1.Designer.cs
+++ Rects/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.изображениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.выходToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.инструментыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выделениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.созданиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.объектToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.изображениеToolStripMenuItem,
+            this.инструментыToolStripMenuItem,
+            this.объектToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(344, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // изображениеToolStripMenuItem
+            // 
+            this.изображениеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.выходToolStripMenuItem,
+            this.выходToolStripMenuItem1});
+            this.изображениеToolStripMenuItem.Name = "изображениеToolStripMenuItem";
+            this.изображениеToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.изображениеToolStripMenuItem.Text = "Изображение";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Enabled = false;
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Enabled = false;
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Enabled = false;
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(129, 6);
+            // 
+            // выходToolStripMenuItem1
+            // 
+            this.выходToolStripMenuItem1.Enabled = false;
+            this.выходToolStripMenuItem1.Name = "выходToolStripMenuItem1";
+            this.выходToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.выходToolStripMenuItem1.Text = "Выход";
+            // 
+            // инструментыToolStripMenuItem
+            // 
+            this.инструментыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выделениеToolStripMenuItem,
+            this.созданиеToolStripMenuItem});
+            this.инструментыToolStripMenuItem.Name = "инструментыToolStripMenuItem";
+            this.инструментыToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.инструментыToolStripMenuItem.Text = "Инструменты";
+            // 
+            // выделениеToolStripMenuItem
+            // 
+            this.выделениеToolStripMenuItem.Enabled = false;
+            this.выделениеToolStripMenuItem.Name = "выделениеToolStripMenuItem";
+            this.выделениеToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.выделениеToolStripMenuItem.Text = "Выделение";
+            // 
+            // созданиеToolStripMenuItem
+            // 
+            this.созданиеToolStripMenuItem.Enabled = false;
+            this.созданиеToolStripMenuItem.Name = "созданиеToolStripMenuItem";
+            this.созданиеToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.созданиеToolStripMenuItem.Text = "Создание";
+            // 
+            // объектToolStripMenuItem
+            // 
+            this.объектToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удалитьToolStripMenuItem});
+            this.объектToolStripMenuItem.Name = "объектToolStripMenuItem";
+            this.объектToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.объектToolStripMenuItem.Text = "Объект";
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Enabled = false;
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(344, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Редактор прямоугольников";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem изображениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem инструментыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выделениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem созданиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem объектToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
     }
 }
 
--- Rects/Form1.cs
+++ Rects/Form1.cs
@@ -7,14 +7,131 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+/*
+ * Создание нового изображения
+ * Создание прямоугольника
+ * Удаление прямоугольника
+ * Изменение положения или размера прямоугольника
+ * Отображении истории изменения изображения (список команд)
+ * Отмена единичной команды (Undo)
+ * Повторение единичной команды (Redo)
+ * Отмена группы команд
+ */
 
 namespace Rects
 {
     public partial class Form1 : Form
     {
+        private Thread t;
+        private volatile Bitmap background = null;
+        private Object painting = new Object();
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private delegate Color Raytracer(double x, double y);
+
+        private Color GetColor(double x, double y, int k)
+        {
+            return Color.FromArgb((int)(x * 255.0), k, (int)(y * 255.0));
+        }
+
+        private void Redraw(int width, int height)
+        {
+            t = new Thread(() =>
+            {
+                var b = new Bitmap(width, height);
+                int green = new Random().Next(256);
+                var g = Graphics.FromImage(b);
+
+                var sw = new Stopwatch();
+                sw.Start();
+                int x, y, z;
+                z = 64;
+                //z = 64;
+                for (z = 64; z >= 2; z /= 2)
+                {
+                    for (y = 0; y < height / z; y++)
+                    {
+                        double dy = y * z / (double)(height - 1);
+                        for (x = 0; x < width / z; x++)
+                        {
+                            double dx = x * z / (double)(width - 1);
+                            var br = new SolidBrush(GetColor(dx, dy, green));
+                            g.FillRectangle(br, x * z, y * z, z, z);
+                        }
+                    }
+                    g.Flush();
+                    Thread.Sleep(200);
+                    lock (painting)
+                    {
+                        background = b;
+                    }
+                    Invalidate();
+                }
+
+                /*lock (painting)
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            b.SetPixel(x, y, GetColor(x / (double)width, y / (double)height, green));
+                        }
+                    }
+
+                    background = b;
+                }
+                Invalidate();*/
+                sw.Stop();
+                Debug.WriteLine("Draw: {0} ms", sw.ElapsedMilliseconds);
+
+
+            });
+            t.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            lock (painting)
+            {
+                if (background == null)
+                    return;
+                /*var sw = new Stopwatch();
+                sw.Start();*/
+                e.Graphics.DrawImageUnscaled(background, 0, 0);
+                /*sw.Stop();
+                Debug.WriteLine("Elapsed: {0}", sw.ElapsedMilliseconds);*/
+            }
+            //BackgroundImage
+            //e.Graphics.DrawImage()
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            t.Abort();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Text = string.Format("{0} {1}", Width, Height);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Redraw(Width, Height);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+                Redraw(Width, Height);
         }
     }
 }
